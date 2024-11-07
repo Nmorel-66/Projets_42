ab555939779b62ea0666a9062a61b31bf158bd44
Nicolas Morel <nimorel@c2r6p1.42perpignan.fr>
2024-11-07 18:02:46 +0100

    ft_memcpy on air

--- ft_memmove.c
+++ ft_memmove.c
@@ -5,46 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nimorel <nimorel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/06 15:20:25 by nimorel           #+#    #+#             */
-/*   Updated: 2024/11/07 15:16:41 by nimorel          ###   ########.fr       */
+/*   Created: 2024/11/07 17:39:57 by nimorel           #+#    #+#             */
+/*   Updated: 2024/11/07 17:47:49 by nimorel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void *memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*s;
-	unsigned char	*d;
 	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
+	unsigned char	*tmp;
 
-	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
 	i = 0;
-	if (dest == src || n == 0)
-		return (dest);
-	if (d > s)
+	while (i < n)
 	{
-		while (n > 0)
-		{
-			n--;
-			d[n] = s[n];
-		}
-	}
-	else
-	{
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
+		d[i] = s[i];
+		i++;
 	}
 	return (dest);
 }
-/* La fonction memmove() copie n octets depuis la zone mémoire src vers la
-zone mémoire dest. Les deux zones  peuvent  se  chevaucher :  la  copie
-s’effectue  comme  si  src  était  premièrement  copiée dans un tableau
-temporaire ne chevauchant pas src ou dest puis il est copié vers  dest
-premiere cond copie de l'arriere vers l'avant pour eviter chvauchement
-2nd cond copie dvers l'avant
-*/
