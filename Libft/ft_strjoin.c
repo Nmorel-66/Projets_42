4acd59be09a071ebadf8e204e9a1ca27c722f66c
Nicolas Morel <nimorel@c2r6p1.42perpignan.fr>
2024-11-08 16:13:31 +0100

    add str_join

---
+++ Libft/ft_strjoin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimorel <nimorel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 15:28:31 by nimorel           #+#    #+#             */
+/*   Updated: 2024/11/08 16:12:56 by nimorel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*new_str;
+	size_t	i;
+	size_t	j;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	new_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (new_str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+/*int	main(void)
+{
+	char *s1 = "Hello ";
+    char *s2 = "world !";
+	char *res = ft_strjoin(s1, s2);
+	printf("res: %s\n", res);
+	free(res);
+	return(0);
+}*/
