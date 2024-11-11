65f0b06840d6409ec6966cd0948ea61fac664e99
Nicolas Morel <nimorel@c3r5p7.42perpignan.fr>
2024-11-11 11:23:58 +0100

    add ft_striteri

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -1,56 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nimorel <nimorel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 09:52:43 by nimorel           #+#    #+#             */
-/*   Updated: 2024/11/11 11:12:53 by nimorel          ###   ########.fr       */
+/*   Created: 2024/11/11 10:49:06 by nimorel           #+#    #+#             */
+/*   Updated: 2024/11/11 11:23:24 by nimorel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
-	char			*dest;
 
 	i = 0;
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (dest == NULL)
-		return (NULL);
 	if (s && f)
 	{
 		while (s[i] != '\0')
 		{
-			dest[i] = f(i, s[i]);
+			f(i, &s[i]);
 			i++;
 		}
 	}
-	dest[i] = '\0';
-	return (dest);
 }
-/*static char    ft_upper(unsigned int i, char c)
+/*static void    ft_upper(unsigned int i, char *c)
 {
     (void)i;
-    return (c - 32);
+    *c = *c - 32;
 }
 int     main(void)
 {
-    char    *str = "hello";
-    char    *result;
+    char    str[5] = "hello";
 
-    result = ft_strmapi(str, &ft_upper);
-    if (result == NULL)
-    {
-        printf("Error: Memory allocation failed.\n");
-        return (1);
-    }
-
-    printf("%s\n", result);
-    free(result);
-
+    ft_striteri(str, &ft_upper);
+	printf("%s\n", str);
     return (0);
 }*/
