786e8049b7f3ae2dc222a1ec7b4cd168aa64f3ed
Nicolas Morel <nimorel@c3r5p7.42perpignan.fr>
2024-11-11 09:51:13 +0100

    add ft_itoa.c

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nimorel <nimorel <marvin@42.fr> >          +#+  +:+       +#+        */
+/*   By: nimorel <nimorel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:59:51 by nimorel           #+#    #+#             */
-/*   Updated: 2024/11/10 18:54:15 by nimorel          ###   ########.fr       */
+/*   Updated: 2024/11/11 09:50:44 by nimorel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 static int	ft_nblen(int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (n < 0)
@@ -27,6 +27,17 @@ static int	ft_nblen(int n)
 		i++;
 	}
 	return (i);
+}
+
+static char	*ft_strnew(int len)
+{
+	char	*dest;
+
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+		return (NULL);
+	dest[0] = '0';
+	return (dest);
 }
 
 char	*ft_itoa(int n)
@@ -41,31 +52,24 @@ char	*ft_itoa(int n)
 	len = ft_nblen(nb);
 	if (nb <= 0)
 		len = len + 1;
-	res = (char *)malloc(sizeof(char) * (len + 1));
-	if (!res)
-		return (NULL);
-	if (nb == 0)
-	{
-		res[0] = '0';
+	res = ft_strnew(len);
+	if (nb == 0 || nb < 0)
 		i++;
-	}
 	if (nb < 0)
 	{
 		res[0] = '-';
 		nb = -nb;
-		i++;
-	}
+	}	
 	while (len > i)
 	{
 		len--;
 		res[len] = (nb % 10) + '0';
-		nb = nb /10;
+		nb = nb / 10;
 	}
 	return (res);
 }
-
-int	main(void)
+/*int	main(void)
 {
-	printf("%s\n", ft_itoa(120));
+	printf("%s\n", ft_itoa(-12));
 	return (0);
-}
+}*/
