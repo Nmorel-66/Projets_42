f00606db07fc43d7b3818d5ae03878524b94d6eb
Nicolas Morel <nicofromperpinya@gmail.com>
2024-11-16 20:40:48 +0100

    modif printf

--- Printf/src/ft_printf.c
+++ Printf/src/ft_printf.c
@@ -3,97 +3,98 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nimorel <nimorel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nimorel <nimorel <marvin@42.fr> >          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 09:47:54 by nimorel           #+#    #+#             */
-/*   Updated: 2024/11/16 15:28:11 by nimorel          ###   ########.fr       */
+/*   Updated: 2024/11/16 20:39:37 by nimorel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <stdio.h>
 #include <unistd.h>
-
+#include <stdio.h>
 
 size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t i = 0;
 
-	i = 0;
-	while (s[i] != '\0')
+	 while (s[i] != '\0')
 		i++;
 	return (i);
 }
+
 int	ft_putstr_fd(char *s, int fd)
 {
 	write(fd, s, ft_strlen(s));
 	return (ft_strlen(s));
 }
+
 int	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 	return (1);
 }
+
 int	ft_putnbr_fd(int n, int fd)
 {
-	long	nb;
+	long nb = n;
+	int len = 0;
 
-	nb = n;
 	if (nb < 0)
 	{
-		ft_putchar_fd('-',fd);
-		nb = nb * (-1);
+		len = len + ft_putchar_fd('-', fd);
+		nb = -nb;
 	}
 	if (nb > 9)
 	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putnbr_fd(nb % 10, fd);
+		len = len + ft_putnbr_fd(nb / 10, fd);
+		len = len + ft_putnbr_fd(nb % 10, fd);
 	}
 	else
-		ft_putchar_fd(nb + 48, fd);
-	return (1);
+		len = len + ft_putchar_fd(nb + '0', fd);
+	return (len);
 }
 
 int ft_printf(const char *format, ...)
 {
-	va_list	args;
-	int		len;  
-	
-	len = 0;
+	va_list args;
+	int len = 0;
+	int i = 0;
+
 	va_start(args, format);
-	 while (format[len]) {
-        if (format[len] == '%')
+	while (format[i])
+	{
+		if (format[i] == '%')
 		{
-           	len++; 
-            if (format[len] == 'd')
+			i++;
+			if (format[i] == 'd')
 				len = len + ft_putnbr_fd(va_arg(args, int), 1);
-			if (format[len] == 'c')
+			else if (format[i] == 'c')
 				len = len + ft_putchar_fd(va_arg(args, int), 1);
-			if (format[len] == 's')
-                len = len + ft_putstr_fd(va_arg(args, char *), 1);             
-        }
-		if (format[len] == '\n')
-			ft_putchar_fd('\n', 1);
+			else if (format[i] == 's')
+				len = len + ft_putstr_fd(va_arg(args, char *), 1);
+			else
+				len = len + ft_putchar_fd(format[i], 1);
+		}
 		else
-			ft_putchar_fd(format[len], 1);
-        len++;
-    }
-	
-    va_end(args);
+			len = len + ft_putchar_fd('%', 1);
+		i++;
+	}
+	va_end(args);
 	return (len);
 }
+
 int main(void)
 {
-    //int	c = 67; // 67 = C;
-	int i = 42;
-	//char *s = "Hello";
-	int	len;
-	
-   	len = 0;
-	//printf("Printf affiche %d\n", i);
-	len = printf("Printf affiche %d\n", i);
-	printf("len%d\n", len);
-    len = ft_printf("printf affiche %d\n", i);
-	printf("len%d\n", len);
-	return (0);
+   	int i = 42;
+	char c = 'A';
+	char *s = "Hello";
+	int len;
+
+	len = printf("Printf affiche %d, %c, %s\n", i, c, s);
+	printf("len (printf): %d\n", len);
+
+	len = ft_printf("ft_printf affiche %d, %c, %s\n", i, c, s);
+	printf("len (ft_printf): %d\n", len);
+    return (0);
 }
