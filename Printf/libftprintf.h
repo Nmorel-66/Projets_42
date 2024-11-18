5a11cdc0a4b78ce5722bb3dff171bdcb8af7f7cc
Nicolas Morel <nicofromperpinya@gmail.com>
2024-11-18 10:57:26 +0100

    save work  18/11

--- Printf/libftprintf.h
+++ Printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: nimorel <nimorel <marvin@42.fr> >          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 10:04:06 by nimorel           #+#    #+#             */
-/*   Updated: 2024/11/18 10:06:06 by nimorel          ###   ########.fr       */
+/*   Updated: 2024/11/18 10:20:08 by nimorel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define LIBFTPRINTF_H
 
 # include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
 int	ft_printchar(int c);
 int	ft_printstr(char *str);
 int	ft_printnbr(int n);
