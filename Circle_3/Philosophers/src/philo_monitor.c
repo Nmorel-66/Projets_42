b9a0b43154296a78893c2e4bc4038539b44285f2
Nicolas Morel <nicofromperpinya@gmail.com>
2025-05-07 15:02:11 +0200

    remove comment

--- Circle_3/Philosophers/src/philo_monitor.c
+++ Circle_3/Philosophers/src/philo_monitor.c
@@ -6,7 +6,7 @@
 /*   By: nimorel <nimorel <marvin@42.fr> >          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 07:44:42 by nimorel           #+#    #+#             */
-/*   Updated: 2025/05/07 14:58:56 by nimorel          ###   ########.fr       */
+/*   Updated: 2025/05/07 15:01:02 by nimorel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,44 +81,3 @@ void	*ft_monitoring(void *arg)
 	}
 	return (NULL);
 }
-
-/*void	*ft_monitoring(void *arg)
-{
-	t_data		*data;
-	int			i;
-	long long	now;
-
-	data = (t_data *)arg;
-	while (1)
-	{
-		i = 0;
-		while (i < data->nb_philos)
-		{
-			pthread_mutex_lock(&data->philos[i].lock);
-			now = ft_gettime();
-			if ((now - data->philos[i].last_meal) > data->time_to_die)
-			{
-				pthread_mutex_lock(&data->state_mutex);
-				data->someone_died = 1;
-				pthread_mutex_lock(&data->write_mutex);
-				printf("\033[1;31m%lld %d 💀 died\n\033[0m",
-					now - data->start_time, data->philos[i].id);
-				pthread_mutex_unlock(&data->write_mutex);
-				pthread_mutex_unlock(&data->state_mutex);
-				pthread_mutex_unlock(&data->philos[i].lock);
-				return (NULL);
-			}
-			pthread_mutex_unlock(&data->philos[i].lock);
-			i++;
-		}
-		if (all_philos_ate(data))
-		{
-			pthread_mutex_lock(&data->state_mutex);
-			data->someone_died = 1;
-			pthread_mutex_unlock(&data->state_mutex);
-			return (NULL);
-		}
-		usleep(1000);
-	}
-	return (NULL);
-}*/
